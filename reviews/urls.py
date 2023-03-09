effc4fbcee93a0e9c8d942ca4632cbaf6d1a523b
Richard Tunstall <ricktunstalljr@gmail.com>
2023-03-08 21:31:38 -0500

    added and updated files

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    path('media/', views.media),
+    path('all/', views.all),
+    path('one/', views.one)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("yello")
+    return render(request, 'dashboard.html')
+
+def media(request):
+    return render(request, 'media.html')
+
+def all(request):
+    return render(request,'all_reviews.html')
+
+def one(request):
+    return render(request, 'one_review.html')
