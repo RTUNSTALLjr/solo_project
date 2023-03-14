2840e2fb8b4e1e728f2892b81e0791d42998d50f
Richard Tunstall <ricktunstalljr@gmail.com>
2023-03-13 22:26:28 -0400

    more new stuff

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    score = models.IntegerField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    score = models.IntegerField()
+    comment = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Upcoming(models.Model):
+    pass
